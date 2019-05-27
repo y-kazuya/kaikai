ea5ca19f6a7a626d4ce96d4b770b4254b6d5ba9c
y-kazuya <y.kazuya.0124@au.com>
2019-05-27 23:45:10 +0900

    ルーティング基礎設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'public/pages#top'
+
+  namespace :admin do
+  end
+
+  scope module: :public do
+
+  end
+
+  namespace :api, {format: 'json'} do
+
+  end
 end
