cddd03af619b0f00c059cbb8cfc2e0a2bb17786b
y-kazuya <y.kazuya.0124@au.com>
2019-05-27 23:16:27 +0900

    rspec導入とジェネレーター時の設定

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,21 @@ module Kaikai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     config.generators do |g|
       g.javascripts false
       g.helper false
-      g.test_framework false
       g.assets false
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
     # Settings in config/environments/* take precedence over those specified here.
