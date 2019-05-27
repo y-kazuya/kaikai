5fc7433bc2debb1e322d489bb95d9268df523ab7
y-kazuya <y.kazuya.0124@au.com>
2019-05-27 22:58:03 +0900

    コントローラーの生成設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module Kaikai
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      g.assets false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
