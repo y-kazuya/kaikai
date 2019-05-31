47bb67258d51cd47c97b1923e132a32b5fcb6f9a
y-kazuya <y.kazuya.0124@au.com>
2019-05-31 21:42:17 +0900

    一般向けアプリケーションコントローラー

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,3 +1,21 @@
 class Public::ApplicationController < ApplicationController
 
+  include SessionsHelper
+
+  private
+    def logged_in_account #ログインしてなかったらログイン画面へリダイレクト
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to new_session_path
+      end
+    end
+
+    def non_logged_in_account #ログインしてたらルートへ
+      if logged_in?
+        flash[:danger] = "ログイン済みです"
+        redirect_to root_path
+      end
+    end
+
 end
