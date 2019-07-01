c96f4b10351f51c0b65672bf14e111c945419169
y-kazuya <y.kazuya.0124@au.com>
2019-07-01 13:34:08 +0900

    その日の履歴が保存されると自動的にcheck_histroyも追加

--- app/models/user_history.rb
+++ app/models/user_history.rb
@@ -1,6 +1,23 @@
 class UserHistory < ApplicationRecord
   belongs_to :user
-
   has_many :user_check_histories, dependent: :destroy
+
+  after_create :make_check_history
+
+
+
+
+
+
+
+  def make_check_history
+    user_check_histories = []
+    self.user.checks.each do |check|
+      user_check_histories << UserCheckHistory.new(user_history_id: self.id, user_check_id: check.id)
+    end
+
+    UserCheckHistory.import user_check_histories
+
+  end
 
 end
