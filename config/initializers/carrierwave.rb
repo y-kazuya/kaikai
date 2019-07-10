527fa28da0aae81958b6b590648929dc0f6c6a0e
y-kazuya <y.kazuya.0124@au.com>
2019-07-11 03:13:06 +0900

    Fo4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      region: 'ap-northeast-1'
+      region: Rails.application.secrets.aws_region
     }
   else
     config.storage :file
