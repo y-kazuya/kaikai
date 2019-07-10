41b0e437f1a3e8cd180867b1045a060fa5743f9a
y-kazuya <y.kazuya.0124@au.com>
2019-07-11 03:16:12 +0900

    Fo5

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,9 +11,9 @@ CarrierWave.configure do |config|
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kaikai-sto'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      region: Rails.application.secrets.aws_region
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV["AWS_REGION"]
     }
   else
     config.storage :file
