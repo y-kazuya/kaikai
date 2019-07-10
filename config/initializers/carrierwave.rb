ff4924bcd99761ee8c0c475ea3031c0f67bb8603
y-kazuya <y.kazuya.0124@au.com>
2019-07-11 03:05:43 +0900

    F03

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,16 +3,22 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.cache_storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.production?
+    config.cache_storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'kaikai-sto'
 
-  config.fog_directory  = 'kaikai-sto'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kaikai-sto'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kaikai-sto'
+
 end
