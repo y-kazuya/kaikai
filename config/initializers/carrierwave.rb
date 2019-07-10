26b4640dc2f7f1933edb4632b3ca93a70c5eddd1
y-kazuya <y.kazuya.0124@au.com>
2019-07-11 02:55:32 +0900

    Fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,6 @@
 CarrierWave.configure do |config|
+  config.cache_storage = :fog
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
@@ -7,7 +9,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'kaikai-sto'
-  config.cache_storage = :fog
-  config.fog_provider = 'fog/aws'
+
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kaikai-sto'
 end
