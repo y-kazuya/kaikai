6f1aea5421feef28178032d484a438b897005888
y-kazuya <y.kazuya.0124@au.com>
2019-07-11 22:10:11 +0900

    Region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,8 @@ CarrierWave.configure do |config|
 
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["AWS_REGION"]
+      # region: 'ap-northeast-1'・
+      region: 'ap-northeast-1'
     }
     config.fog_directory  = 'kaikai-sto'
     # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kaikai-sto'
